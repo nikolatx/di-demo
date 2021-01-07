eee04d9a3c01313db9476772e5cfaffd7d08f6e3
Nikola Timotijevic <nikolatx@gmail.com>
2021-01-07 19:50:26 +0100

    Get bean

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -1,13 +1,22 @@
 package guru.springframework.didemo;
 
+import guru.springframework.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = {"guru.springframework.controllers"})
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+        MyController controller = (MyController) ctx.getBean("myController");
+        controller.hello();
     }
 
 }
